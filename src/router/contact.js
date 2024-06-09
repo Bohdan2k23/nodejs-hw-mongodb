180f2f26a9d4ef0d36c6cafd4b1b8a030a897acc
Bohdan2k23 <130351031+Bohdan2k23@users.noreply.github.com>
2024-06-09 14:26:15 +0200

    fix bug

--- src/router/contact.js
+++ src/router/contact.js
@@ -4,25 +4,29 @@ import { getAllContacts, getContactById } from "../services/contacts.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const data = await getAllContacts();
+  try {
+    const data = await getAllContacts();
 
-  if (data.length) {
-    res.status(200).json({ status: 200, messege: "Successfully found contacts!", data });
-  } else {
-    res.status(404).json({ status: 404, message: "No contacts found", data });
+    if (data.length) {
+      res.status(200).json({ status: 200, messege: "Successfully found contacts!", data });
+    } else {
+      res.status(404).json({ status: 404, message: "No one contact", data });
+    }
+  } catch (error) {
+    res.status(404).json({ status: 404, message: "Something went wrong", data: null });
   }
 });
 
 router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const data = await getContactById(id);
+  try {
+    const { id } = req.params;
+    const data = await getContactById(id);
 
-  if (data) {
     res
       .status(200)
       .json({ status: 200, messege: `Successfully found contact with id ${id}!`, data });
-  } else {
-    res.status(404).json({ status: 404, message: "Contact not found", data });
+  } catch (error) {
+    res.status(404).json({ status: 404, message: "Contact not found", data: null });
   }
 });
 
